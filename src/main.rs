8f27e994632aaa5a1bacd81fd440edd1ccb08299
uzakotim <timauzakov@gmail.com>
2025-10-03 16:41:23 +0500

    added urls shortener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use reqwest::blocking::Client;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let client = Client::new();
+
+    if args[1] == "create" {
+        let url = &args[2];
+        let res: serde_json::Value = client
+            .post("https:/shtly.ddns.net/api/shorten")
+            .json(&serde_json::json!({ "originalUrl": url }))
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+        println!("Shortened URL: {}", res["shortUrl"]);
+    }
 }
